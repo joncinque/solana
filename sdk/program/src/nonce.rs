4daa6e452590be5c2121eb2b721d8205473c63d1
Jon C <me@jonc.dev>
2024-11-05 12:27:26 +0100

    sdk: Add deprecation warning to `solana_program::nonce`
    #### Problem
    
    As pointed out at
    https://github.com/anza-xyz/agave/pull/3370#issuecomment-2447743597,
    the solana-nonce crate re-export in solana-program does not have a
    deprecated warning.
    
    #### Summary of changes
    
    Add the warning.

--- sdk/program/src/nonce.rs
+++ sdk/program/src/nonce.rs
@@ -1,5 +1,7 @@
+#[deprecated(since = "2.1.0", note = "Use `solana-nonce` crate instead")]
 pub use solana_nonce::{state::State, NONCED_TX_MARKER_IX_INDEX};
 pub mod state {
+    #[deprecated(since = "2.1.0", note = "Use `solana-nonce` crate instead")]
     pub use solana_nonce::{
         state::{Data, DurableNonce, State},
         versions::{AuthorizeNonceError, Versions},
