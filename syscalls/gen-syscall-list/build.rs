6436b561a7ae9da9f3717ea1b9cf3bf11cd7e043
Jon C <me@jonc.dev>
2025-12-01 23:28:27 +0100

    build-sbf: Fix syscalls.txt, inline them
    #### Problem
    
    The syscalls generation script is broken, and isn't actually generating
    anything in syscalls.txt.
    
    Also, syscalls.txt is currently used from the SBF SDK directory, but we
    could simply embed them in cargo-build-sbf directly.
    
    #### Summary of changes
    
    First, fix the regex in gen-syscall-list so that it covers normal and
    feature-gated syscalls.
    
    Next, make gen-syscall-list also generate a syscalls.rs file for
    cargo-build-sbf, and use that directly in cargo-build-sbf. With #9345,
    there won't be any more usage of SBF SDK in cargo-build-sbf.

--- syscalls/gen-syscall-list/build.rs
+++ syscalls/gen-syscall-list/build.rs
@@ -16,10 +16,13 @@ use {
 fn main() {
     let syscalls_rs_path = PathBuf::from("../src/lib.rs");
     let syscalls_txt_path = PathBuf::from("../../platform-tools-sdk/sbf/syscalls.txt");
+    let build_sbf_syscalls_path =
+        PathBuf::from("../../platform-tools-sdk/cargo-build-sbf/src/syscalls.rs");
     println!(
-        "cargo:warning=(not a warning) Generating {1} from {0}",
+        "cargo:warning=(not a warning) Generating {1} and {2} from {0}",
         syscalls_rs_path.display(),
-        syscalls_txt_path.display()
+        syscalls_txt_path.display(),
+        build_sbf_syscalls_path.display(),
     );
 
     let mut file = match File::open(&syscalls_rs_path) {
@@ -29,14 +32,29 @@ fn main() {
     let mut text = vec![];
     file.read_to_end(&mut text).unwrap();
     let text = str::from_utf8(&text).unwrap();
-    let file = match File::create(&syscalls_txt_path) {
+    let txt_file = match File::create(&syscalls_txt_path) {
         Ok(x) => x,
         Err(err) => panic!("Failed to create {}: {}", syscalls_txt_path.display(), err),
     };
-    let mut out = BufWriter::new(file);
-    let sysc_re = Regex::new(r#"register_syscall_by_name\([[:space:]]*b"([^"]+)","#).unwrap();
+    let mut txt_out = BufWriter::new(txt_file);
+    let rs_file = match File::create(&build_sbf_syscalls_path) {
+        Ok(x) => x,
+        Err(err) => panic!("Failed to create {}: {}", syscalls_txt_path.display(), err),
+    };
+    let mut rs_out = BufWriter::new(rs_file);
+    writeln!(rs_out, "pub(crate) const SYSCALLS: &[&str] = &[").unwrap();
+    let sysc_re = Regex::new(r#"register_function\([[:space:]]*"([^"]+)","#).unwrap();
     for caps in sysc_re.captures_iter(text) {
         let name = caps[1].to_string();
-        writeln!(out, "{name}").unwrap();
+        writeln!(txt_out, "{name}").unwrap();
+        writeln!(rs_out, "    \"{name}\",").unwrap();
     }
+    let feature_gate_syscall_re =
+        Regex::new(r#"register_feature_gated_function!\([^"]+"([^"]+)","#).unwrap();
+    for caps in feature_gate_syscall_re.captures_iter(text) {
+        let name = caps[1].to_string();
+        writeln!(txt_out, "{name}").unwrap();
+        writeln!(rs_out, "    \"{name}\",").unwrap();
+    }
+    writeln!(rs_out, "];").unwrap();
 }
